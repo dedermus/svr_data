c473d390745e267779a18457809cc3a17b33b301
Alexander.L <lomovtsev.ab@plinor.ru>
2024-10-22 15:01:37 +0300

    delop

---
+++ database/migrations/2024_01_02_000001_create_svr_data_schemas.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+use Svr\Core\Traits\PostgresGrammar;
+
+return new class extends Migration {
+
+    use PostgresGrammar;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $this->enumExists();
+
+        DB::statement('CREATE SCHEMA IF NOT EXISTS data');
+        DB::statement("COMMENT ON SCHEMA system IS 'Основная схема'");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement('DROP SCHEMA IF EXISTS data CASCADE');
+    }
+};
