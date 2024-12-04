b763c78e3889d0bed98b304441838d0bc25f7677
Ilya.F <ilja.filin2010@yandex.ru>
2024-12-04 16:11:36 +0300

    метод animals\keeping_object_edit

--- routes/Api/api.php
+++ routes/Api/api.php
@@ -23,9 +23,9 @@ Route::prefix(config('svr.api_prefix'))->group(function(){
     Route::get('animals/list', [ApiAnimalsController::class, 'animalsList'])->middleware(['auth:svr_api', 'api']);
     Route::post('animals/mark_edit', [ApiAnimalsController::class, 'animalsMarkEdit'])->middleware(['auth:svr_api', 'api']);
     Route::post('animals/mark_edit_group', [ApiAnimalsController::class, 'animalsMarkEditGroup'])->middleware(['auth:svr_api', 'api']);
-//    Route::post('animals/mark_photo_edit', [ApiAnimalsController::class, 'animalsMarkPhotoEdit'])->middleware(['auth:svr_api', 'api']);
-//    Route::post('animals/mark_photo_delete', [ApiAnimalsController::class, 'animalsMarkPhotoDelete'])->middleware(['auth:svr_api', 'api']);
-//    Route::post('animals/animal_keeping_object_edit', [ApiAnimalsController::class, 'animalsKeepingObjectEdit'])->middleware(['auth:svr_api', 'api']);
+    Route::post('animals/mark_photo_edit', [ApiAnimalsController::class, 'animalsMarkPhotoEdit'])->middleware(['auth:svr_api', 'api']);
+    Route::post('animals/mark_photo_delete', [ApiAnimalsController::class, 'animalsMarkPhotoDelete'])->middleware(['auth:svr_api', 'api']);
+    Route::post('animals/animal_keeping_object_edit', [ApiAnimalsController::class, 'animalsKeepingObjectEdit'])->middleware(['auth:svr_api', 'api']);
 //    Route::post('animals/animal_keeping_object_edit_group', [ApiAnimalsController::class, 'animalsKeepingObjectEditGroup'])->middleware(['auth:svr_api', 'api']);
 //    Route::post('animals/animal_birth_object_edit', [ApiAnimalsController::class, 'animalsBirthObjectEdit'])->middleware(['auth:svr_api', 'api']);
 });
